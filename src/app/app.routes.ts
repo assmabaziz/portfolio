43358b321991a541bfdf532f0d2b0586895cec35
assmabaziz <assma.baziz@yahoo.com>
2025-07-11 14:52:34 +0300

    routes added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,28 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { EducationComponent } from './education/education.component';
+import { SkillsComponent } from './skills/skills.component';
+import { ContactComponent } from './contact/contact.component';
+import { MeComponent } from './me/me.component';
+import { ProjectsComponent } from './projects/projects.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:"Home", component: HomeComponent,
+        title:"Home"
+    },
+     {path:"Education", component: EducationComponent,
+        title:"Education"
+    },
+     {path:"Skills", component: SkillsComponent,
+        title:"Skills"
+    },
+     {path:"Contact", component: ContactComponent,
+        title:"Contact"
+    },
+     {path:"Me", component: MeComponent,
+        title:"Me"
+    },
+     {path:"Projects", component: ProjectsComponent,
+        title:"Projects"
+    }
+];
